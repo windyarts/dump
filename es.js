d22601f5745f0ebe11ae620f5c0a888e0ed9fe79
Ziming Miao <miaoziming@gmail.com>
2015-04-27 12:16:05 +0800

    Try to resolve mem leak

--- es.js
+++ es.js
@@ -40,56 +40,70 @@ function send() {
     return Q.allSettled([sendToVanilla(), sendToOptimized()]).catch(console.log).then(send);
 }
 
+var vanilla = {
+    index: 'vanilla',
+    type: null,
+    body: {
+        "eventType": null,
+        "serverTimestamp": "2014-06-26T22:19:08.000000-00:00",
+        "data": {
+           'whatever': 'xxxx'
+        },
+        "build": "1.1.1",
+        "productKey": "08d2bdd9-631d-4622-bfd7-6654df871d52",
+        "schemaHash": null,
+        "ipAddress": "106.187.101.105",
+        "sessionId": "fff324c6-30a2-11e3-ad80-485d60066bda",
+        "clientTimestamp": "2014-06-26T22:19:08.000000-00:00",
+        "userId": null,
+        "device": "Utility",
+        "userName": null
+    }
+};
+
 function sendToVanilla() {
     var type = etype();
     var userId = uuid();
     console.log('vanilla', type);
-    return client.index({
-        index: 'vanilla',
-        type: type,
-        body: {
-            "eventType": type,
-            "serverTimestamp": "2014-06-26T22:19:08.000000-00:00",
-            "data": {
-               'whatever': 'xxxx'
-            },
-            "build": "1.1.1",
-            "productKey": "08d2bdd9-631d-4622-bfd7-6654df871d52",
-            "schemaHash": type.split('|')[1],
-            "ipAddress": "106.187.101.105",
-            "sessionId": "fff324c6-30a2-11e3-ad80-485d60066bda",
-            "clientTimestamp": "2014-06-26T22:19:08.000000-00:00",
-            "userId": userId,
-            "device": "Utility",
-            "userName": "Player" + userId
-        }
-    });
+    vanilla.type = type;
+    vanilla.body.eventType = type;
+    vanilla.body.schemaHash = type.split('|')[1];
+    vanilla.body.userId = userId;
+    vanilla.body.userName = "Player" + userId;
+    return client.index(vanilla);
 }
+
+var optimized = {
+    index: 'optimized',
+    type: null,
+    body: {
+        "eventType": null,
+        "serverTimestamp": "2014-06-26T22:19:08.000000-00:00",
+        "data": {
+           'whatever': 'xxxx'
+        },
+        "build": "1.1.1",
+        "productKey": "08d2bdd9-631d-4622-bfd7-6654df871d52",
+        "schemaHash": null,
+        "ipAddress": "106.187.101.105",
+        "sessionId": "fff324c6-30a2-11e3-ad80-485d60066bda",
+        "clientTimestamp": "2014-06-26T22:19:08.000000-00:00",
+        "userId": null,
+        "device": "Utility",
+        "userName": null
+    }
+};
 
 function sendToOptimized() {
     var type = etype();
     var userId = uuid();
     console.log('optimized', type);
-    return client.index({
-        index: 'optimized',
-        type: type,
-        body: {
-            "eventType": type,
-            "serverTimestamp": "2014-06-26T22:19:08.000000-00:00",
-            "data": {
-               'whatever': 'xxxx'
-            },
-            "build": "1.1.1",
-            "productKey": "08d2bdd9-631d-4622-bfd7-6654df871d52",
-            "schemaHash": type.split('|')[1],
-            "ipAddress": "106.187.101.105",
-            "sessionId": "fff324c6-30a2-11e3-ad80-485d60066bda",
-            "clientTimestamp": "2014-06-26T22:19:08.000000-00:00",
-            "userId": userId,
-            "device": "Utility",
-            "userName": "Player" + userId
-        }
-    });
+    optimized.type = type;
+    optimized.body.eventType = type;
+    optimized.body.schemaHash = type.split('|')[1];
+    optimized.body.userId = userId;
+    optimized.body.userName = "Player" + userId;
+    return client.index(optimized);
 }
 
 function uuid() {
